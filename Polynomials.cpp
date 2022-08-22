4e39e25c16f32807bcad862e518c3f376e8b8a74
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-08-22 22:46:08 +0530

    Poly using arrays

---
+++ Polynomials.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct Polynomial
+{
+    int coeff;
+    int exp;
+};
+//Polynomial representation using arrays
+// Addition of polynomials
+int add(struct Polynomial P1,struct Polynomial P2,int h1,int h2)
+{
+
+}
+int main()
+{
+   struct Polynomial P1[10],P2[10];
+   
+   
+
+return 0;
+}
