e26d5eeb0d64f6d6681c2ef00635d0282a3f4f81
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-08-26 21:49:21 +0530

    Final addition program

--- Polynomials.cpp
+++ Polynomials.cpp
@@ -4,45 +4,44 @@ struct Polynomial
 {
     int coeff;
     int exp;
-} P1[10], P2[10], P3[10];
+};
 // void display_polynomial(struct Polynomial P[], int k)
 // {
 // }
 // Polynomial representation using arrays
 //  Addition of polynomials
-void add(struct Polynomial P1[], struct Polynomial P2[], int h1, int h2)
+int add(struct Polynomial P1[], struct Polynomial P2[], struct Polynomial P3[], int h1, int h2)
 {
     int i, j, k;
     i = j = k = 0;
     while (i < h1 && j < h2)
     {
-        if (P1[i].exp == P2[j].exp)
+        if (P1[i].exp < P2[j].exp)
         {
-            P3[k].coeff = P1[i].exp + P2[j].exp;
-            P3[k].exp = P1[i].exp;
-            i++;
+            P3[k].coeff = P2[j].coeff;
+            P3[k].exp = P2[j].exp;
             j++;
             k++;
         }
-
         else if (P1[i].exp > P2[j].exp)
         {
-            P3[k].coeff = P1[i].exp;
+            P3[k].coeff = P1[i].coeff;
             P3[k].exp = P1[i].exp;
             i++;
             k++;
         }
         else
         {
-            P3[k].coeff = P2[j].exp;
-            P3[k].exp = P2[j].exp;
+            P3[k].coeff = P1[i].coeff + P2[j].coeff;
+            P3[k].exp = P1[i].exp;
+            i++;
             j++;
             k++;
         }
     }
     while (i < h1)
     {
-        P3[k].coeff = P1[i].exp;
+        P3[k].coeff = P1[i].coeff;
         P3[k].exp = P1[i].exp;
         i++;
         k++;
@@ -54,21 +53,18 @@ void add(struct Polynomial P1[], struct Polynomial P2[], int h1, int h2)
         j++;
         k++;
     }
-    cout << "Resultant Polynomial: " << endl;
-    for (int i = 0; i <= k; i++)
-    {
-        cout << "Co-eff:" << P3[i].coeff << "  Exp:" << P3[i].exp << endl;
-    }
+    return k;
 }
 int main()
 {
+    struct Polynomial P1[10], P2[10], P3[10];
     int h1, h2, i;
-    cout << "Enter the highest degree of polynomial 1" << endl;
+    cout << "Enter no of terms in polynomial 1" << endl;
     cin >> h1;
-    cout << "Enter the highest degree of polynomial 2" << endl;
+    cout << "Enter no of terms in polynomial 2" << endl;
     cin >> h2;
     cout << "For Polynomial 1" << endl;
-    for (i = 0; i <= h1; i++)
+    for (i = 0; i < h1; i++)
     {
         cout << "Enter exponent : " << endl;
         cin >> P1[i].exp;
@@ -76,13 +72,18 @@ int main()
         cin >> P1[i].coeff;
     }
     cout << "For Polynomial 2" << endl;
-    for (i = 0; i <= h1; i++)
+    for (i = 0; i <h1; i++)
     {
         cout << "Enter exponent : " << endl;
         cin >> P2[i].exp;
         cout << "Enter coefficient: " << endl;
         cin >> P2[i].coeff;
     }
-    add(P1, P2, h1, h2);
+    int t = add(P1, P2, P3, h1, h2);
+    cout << "Resultant Polynomial: " << endl;
+    for (int i = 0; i < t; i++)
+    {
+        cout << "Exp:" << P3[i].exp << "  Co-eff:" << P3[i].coeff << endl;
+    }
     return 0;
 }
