a80b36d6a2ddabfc57787ab2d3107b65f662cac9
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-09-17 11:10:15 +0530

    Quick sort

--- array_sorting_methods.cpp
+++ array_sorting_methods.cpp
@@ -9,30 +9,88 @@ private:
     int length;
 
 public:
-    array() { std::cout << "Default constructor" << endl; }
-    array(int len, int s);
+    array();
     ~array();
+    int get_length();
+    void enter_ele();
+    void print_arr();
     void bubble_sort();
     void insertion_sort();
-    void quick_sort();
+    void quick_sort(int l, int h);
     void merge_sort();
     void heap_sort();
     void count_sort();
     void wave_sort();
+    int partition(int l, int h);
     void DNF_sort();
 };
+int array::get_length()
+{
+    return length;
+}
+void array::enter_ele()
+{
+    cout << "Enter elements in array: " << endl;
+    for (int i = 0; i < length; i++)
+    {
+        cin >> a[i];
+    }
+}
+void array::print_arr()
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+int array::partition(int l, int h)
+{
+    int i = 0;
+    int j = length;
+    int pivot = a[l];
+    do
+    {
+        do
+        {
+            i++;
+        } while (a[i] > pivot);
 
+        do
+        {
+            j--;
+        } while (a[j] <= pivot);
+        if (i < j)
+        {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    } while (i < j);
+    if (j > i)
+    {
+        int k = a[l];
+        a[l] = a[j];
+        a[j] = k;
+    }
+
+    return j;
+}
 void array::insertion_sort()
 {
-
 }
-void array::quick_sort()
+void array::quick_sort(int l, int h)
 {
-
+    if (l < h)
+    {
+        int j = partition(l, h);
+        quick_sort(l, j - 1);
+        quick_sort(j + 1, h);
+    }
 }
+
 void array::bubble_sort()
 {
-    cout<< "Bubble sorting array" << endl;
+    cout << "\nBubble sorting array" << endl;
     for (int i = 0; i < length - 1; i++)
     {
         for (int j = i + 1; j < length; j++)
@@ -46,21 +104,41 @@ void array::bubble_sort()
         }
     }
 }
-array::array(int len, int s)
+array::array()
 {
     std::cout << "Constructor called" << endl;
+    cout << "Enter total size of array that you want to create: " << endl;
+    int s;
+    cin >> s;
     size = s;
+    cout << "Enter how many elements you want to enter in array: " << endl;
+    int len;
+    cin >> len;
     length = len;
-    a = new int[size];
+    if (length <= size)
+    {
+        a = new int[size];
+    }
+    else
+    {
+        cout << "Length of array exceeds size" << endl;
+    }
 }
 array::~array()
 {
-    std::cout << "Destrcutor called" << endl;
+    std::cout << "\nDestructor called" << endl;
     delete a;
 }
 
 int main()
 {
-    array First(2, 5);
+    array First;
+    First.enter_ele();
+    First.print_arr();
+    // First.bubble_sort();
+   
+    First.quick_sort(0,( First.get_length()));
+    First.print_arr();
+
     return 0;
 }
