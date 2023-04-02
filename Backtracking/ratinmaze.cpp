06e4cd16183bb3f72deecd304d50c3a6d4ef5f89
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2023-04-02 13:12:14 +0530

    backtracking and DP added

---
+++ Backtracking/ratinmaze.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define N 4
+int maze[N][N] = {{1, 0, 0, 0}, {1, 1, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+int visited[N][N] = {0};
+int count=0;
+
+int main()
+{
+
+    return 0;
+}
